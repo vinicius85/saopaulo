35490ad8c4571fcf0b1924512d19971b7333ae24
Vinicius <vinicius.remigio@gmail.com>
2017-03-07 00:36:37 -0300

    indexacao de info sobre acoes

--- app/collect/src/fundamentalist.py
+++ app/collect/src/fundamentalist.py
@@ -9,14 +9,24 @@ def load_input(filename):
   companies = [line.strip() for line in open(filename, 'r')]
   return companies
 
+def load_companies_info(filename):
+  dict = {}
+  companies = [line.strip() for line in open(filename, 'r')]
+  for company in companies:
+    companyData = company.split(';')
+    dict.update({companyData[0]:{'nome' :    companyData[1],
+                                 'setor':    companyData[2],
+                                 'subsetor': companyData[3],
+                                 'segmento': companyData[4]}})
+  return dict
+
 
 if __name__ == '__main__':
 
     es = Elasticsearch()
     es.indices.create(index='fundamental', ignore=400)
 
-    mainStocks = load_input('/home/vinicius/workspace/saopaulo/app/collect/res/empresas.txt')
-
+    mainStocks = load_companies_info('/home/vinicius/workspace/saopaulo/app/collect/res/empresas.txt')
     lista = fundamentus.get_data()
     
     for k, v in lista.items():
@@ -24,6 +34,10 @@ if __name__ == '__main__':
           es.index(index="fundamental", id=k,  doc_type="stock", \
             body={
               "papel" : k, 
+              "nome" : mainStocks.get(k)['nome'],
+              "setor" : mainStocks.get(k)['setor'],
+              "subsetor" : mainStocks.get(k)['subsetor'],
+              "segmento" : mainStocks.get(k)['segmento'],
               "cotacao" : percentageToFloat(v['cotacao']), 
               "pl" : percentageToFloat(v['P/L']), 
               "p_vpa" : percentageToFloat(v['P/VP']), 
