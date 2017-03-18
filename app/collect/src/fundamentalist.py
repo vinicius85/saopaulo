f2d2c9ced044401e9dd5f9e21dbecc8767425c3a
Vinicius <vinicius.remigio@gmail.com>
2017-03-17 23:49:49 -0300

    adicionando informacoes da carteira

--- app/collect/src/fundamentalist.py
+++ app/collect/src/fundamentalist.py
@@ -1,14 +1,9 @@
 from fundamentus import fundamentus
 from elasticsearch import Elasticsearch
 from datetime import datetime
-from math import log
 
 def percentageToFloat(value):
   return float(value.replace('%','').replace(',','.'))
-
-def load_input(filename):
-  companies = [line.strip() for line in open(filename, 'r')]
-  return companies
 
 def load_companies_info(filename):
   dict = {}
@@ -21,12 +16,27 @@ def load_companies_info(filename):
                                  'segmento': companyData[4]}})
   return dict
 
+def load_my_stocks(filename):
+  dict = {}
+  companies = [line.strip() for line in open(filename, 'r')]
+  for company in companies:
+    companyData = company.split(';')
+    dict.update({companyData[0]:{
+                                 'posicao':     int(companyData[1]),
+                                 'preco_compra': float(companyData[2]),
+                                 'data_inicio': companyData[3],
+                                 'data_aporte': companyData[4]}})
+  return dict
+
 
 def compute_score(att):
   p_vpa = percentageToFloat(att['P/VP']) 
   d_y = percentageToFloat(att['DY']) 
   roe = percentageToFloat(att['ROE']) 
   return   (- 10 * p_vpa) + (2 * d_y) + roe
+
+def rentabilidade(cotacaoEntrada,cotacaoAtual):
+  return (cotacaoAtual-cotacaoEntrada)/100
          
 
 
@@ -36,10 +46,9 @@ if __name__ == '__main__':
     es.indices.create(index='fundamental', ignore=400)
 
     mainStocks = load_companies_info('/home/vinicius/workspace/saopaulo/app/collect/res/empresas.txt')
+    myStocks = load_my_stocks('/home/vinicius/workspace/saopaulo/app/collect/res/carteira.txt')
     lista = fundamentus.get_data()
 
-    ### Incluir carteira
-    
     for k, v in lista.items():
         if(k in mainStocks):
           es.index(index="fundamental", id=k,  doc_type="stock", \
@@ -64,6 +73,11 @@ if __name__ == '__main__':
               'div_brut_patr' :  percentageToFloat(v['Div.Brut/Pat.']),
               "indexUpdate": datetime.now(),
               "p_score" : compute_score(v),
+              "posicao" :  myStocks.get(k)['posicao'] if myStocks.get(k) is not None else 0,
+              "preco_compra" :  myStocks.get(k)['preco_compra'] if myStocks.get(k) is not None else 0.0,
+              "data_inicio" : datetime.strptime(myStocks.get(k)['data_inicio'],'%d/%m/%Y') if myStocks.get(k) is not None else None,
+              "data_aporte" : datetime.strptime(myStocks.get(k)['data_aporte'],'%d/%m/%Y') if myStocks.get(k) is not None else None,
+              "rentabilidade" : rentabilidade(myStocks.get(k)['preco_compra'],percentageToFloat(v['cotacao'])) if myStocks.get(k) else 0,
               'cres_5a' : percentageToFloat(v['Cresc.5a'])}\
           )
 
---
+++ app/fii/README.md
@@ -0,0 +1,29 @@
+FII
+95% do lucro por obrigação
+Fundo de reserva - pagar vacancia
+Boletim mensal BMFBovespa
+índice IFIX
+Liquidez - precisa alguem que compre, assim como acoes
+Definir estrategia
+FFCI11,BCRI11,HGLG11,HGBS11
+Rendimento / Preço de fechamento (cota): Preço bom: acima de 0,60% mes
+Site do bolsoforte
+Valor patrimonial - auditado a cada ano
+
+Checklist
+- Objetivo do Fundo
+ - Laboratorio, hospital, alphaville (FORA!!!)
+- Liquidez
+ - Olhar na boleta
+- Valor Patrominal (P/VPA: 1,35 Acima é caro)
+- Historico de rendimentos
+ - Redimento/Cota
+ - Distribuição: 0.36% ruim
+- Diversificação
+- Histórico de emissão de cotas
+
+Ranking: http://www.clubefii.com.br/fundos_imobiliarios_ranking
+
+SELIC X IFIX -> Selic cai, aumenta IFIX
+
+Nao comprar na baixa muito da selic, pois o fundo ja vai ta precificado irracionalmente
